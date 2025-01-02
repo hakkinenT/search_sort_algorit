4a8811d9929ad2399bb2a520f8458cb483dfe077
Tawanna <thol_16@hotmail.com>
2025-01-02 16:01:23 -0300

    search and sort algorithms

---
+++ src/main/java/com/example/TimeTest.java
@@ -0,0 +1,22 @@
+package com.example;
+
+import java.time.Instant;
+import java.time.Duration;
+
+
+public class TimeTest {
+    static void myFunction() {
+		double x = 10;
+		for (long i = 0; i < 100000000L; i++) {
+			x = -x;
+		}
+	}
+	
+	public static void main(String[] args) {
+		Instant start = Instant.now();
+		myFunction();
+		Instant end = Instant.now();
+		long time = Duration.between(start, end).toMillis();
+		System.out.printf("Duration: %dms\n", time);
+	}
+}
